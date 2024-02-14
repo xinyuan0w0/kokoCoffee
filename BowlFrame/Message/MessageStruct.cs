f8161ac8acc9377c487f9c80eabd05baa2e4f6a7
昕园 <2269296784@qq.com>
2024-02-15 02:10:41 +0800

    添加 消息类

---
+++ BowlFrame/Message/MessageStruct.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlFrame.Message
+{
+    public enum MetaType
+    {
+        /// <summary>
+        /// 通用字段
+        /// </summary>
+        Normal = 0,
+
+        /// <summary>
+        /// 拓展字段(用于适配器传递额支持的内容)
+        /// </summary>
+        Extra = 1,
+
+        /// <summary>
+        /// 自定义字段(用于插件传递内容)
+        /// </summary>
+        Custom = 2
+    }
+
+    public struct MessageBlock
+    {
+        public string Name { get; set; }
+
+        public MetaType MetaType { get; set; }
+
+        public object Value { get; set; }
+
+        /// <summary>
+        /// 可能存在多个同样的字段
+        /// </summary>
+        public bool HaveMulit { get; set; }
+    }
+}
---
+++ BowlFrame/Message/Messages.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlFrame.Message
+{
+    public class Messages
+    {
+        private readonly List<MessageBlock> messageBlocks = new();
+
+        public bool Add(ArraySegment<MessageBlock> messageBlocks)
+        {
+            foreach (MessageBlock messageBlock in messageBlocks)
+                if (!Add(messageBlock))
+                    return false;
+            return true;
+        }
+
+        public bool Add(MessageBlock messageBlock)
+        {
+            messageBlock.HaveMulit = IsMulitBlock(messageBlock);
+
+            if (!messageBlock.HaveMulit)
+                if (GetIndex(messageBlock.Name) is not null)
+                    return false;
+
+            messageBlocks.Add(messageBlock);
+            return true;
+        }
+
+        public List<MessageBlock> GetAll(string name)
+        {
+            List<MessageBlock> result = new();
+
+            int? index = 0;
+            while (true)
+            {
+                index = GetIndex(name, (int)index) + 1;
+                if (index is not null)
+                    result.Add(messageBlocks[(int)index]);
+                else
+                    break;
+            }
+
+            return result;
+        }
+
+        public MessageBlock? Get(int index)
+        {
+            if (index < messageBlocks.Count)
+                return messageBlocks[index];
+            return null;
+        }
+
+        public MessageBlock? Get(string name, int startIndex = 0)
+        {
+            int? index = GetIndex(name, startIndex);
+
+            if (index is null) return null;
+            return messageBlocks[(int)index];
+        }
+
+        public int? GetIndex(string name, int startIndex = 0)
+        {
+            for (int i = startIndex; i < messageBlocks.Count; i++)
+            {
+                if (messageBlocks[i].Name == name)
+                    return i;
+            }
+            return null;
+        }
+
+        public bool DelAll(string name)
+        {
+            int? result;
+            while (true)
+            {
+                result = GetIndex(name);
+                if (result is null)
+                    break;
+
+                if (!DelIndex((int)result))
+                    return false;
+            }
+            return true;
+        }
+
+        public bool Del(string name, int startIndex = 0)
+        {
+            int? result = GetIndex(name, startIndex);
+            if (result is null)
+                return false;
+            return DelIndex((int)result);
+        }
+
+        public bool DelIndex(int index)
+        {
+            if (index >= messageBlocks.Count)
+                return false;
+
+            messageBlocks.RemoveAt(index);
+            return true;
+        }
+
+        public static bool IsMulitBlock(MessageBlock messageBlock)
+        {
+            //部分字段不允许重复内容
+            if (messageBlock.Name == "Text" || messageBlock.Name == "AtBot" || messageBlock.Name == "AtAll")
+                messageBlock.HaveMulit = false;
+
+            return messageBlock.HaveMulit;
+        }
+    }
+}
