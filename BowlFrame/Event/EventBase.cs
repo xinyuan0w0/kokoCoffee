db57a2d251340cbb21bdf4ea84b400365650c91c
昕园 <2269296784@qq.com>
2024-02-15 02:10:27 +0800

    添加 事件类

---
+++ BowlFrame/Event/EventBase.cs
@@ -0,0 +1,30 @@
+﻿using BowlFrame.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlFrame.Event
+{
+    public abstract class EventBase : IEvent
+    {
+        public Adapter.IPlatform Platform { get; }
+
+        public MsgType MsgType { get; }
+
+        public SourceType SourceType { get; }
+
+        public DateTime Time { get; }
+
+        public EventBase(Adapter.IPlatform platform, MsgType msgType, SourceType sourceType, DateTime? time = null)
+        {
+            Platform = platform;
+            MsgType = msgType;
+            SourceType = sourceType;
+            Time = time ?? DateTime.UtcNow;
+        }
+
+        public Messages? Messages { get; }
+    }
+}
---
+++ BowlFrame/Event/Message/PrivateMessageBase.cs
@@ -0,0 +1,16 @@
+﻿using BowlFrame.Adapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlFrame.Event.Message
+{
+    internal abstract class PrivateMessageBase : MessageBase
+    {
+        protected PrivateMessageBase(IPlatform platform) : base(platform, SourceType.Private)
+        {
+        }
+    }
+}
