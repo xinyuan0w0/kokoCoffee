6d90ca7a2c7757edf7bc42b85f76a5c17417b849
昕园 <2269296784@qq.com>
2024-02-29 21:04:30 +0800

    添加 数据库 类

---
+++ BowlFrame/Database/Struct/PlatformIDStruct.cs
@@ -0,0 +1,24 @@
+﻿using SqlSugar;
+
+namespace BowlFrame.Database.Struct
+{
+#pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
+
+    [SugarTable("platformid")]
+    public class Database_PlatformID
+    {
+        [SugarColumn(IsPrimaryKey = true)]
+        public string UUID { get; set; }
+
+        /// <summary>
+        /// 取决于适配器返回的平台名
+        /// </summary>
+        public string Platform { get; set; }
+
+        public string? FatherUUID { get; set; }
+
+        public string ID { get; set; }
+    }
+
+#pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
+}
