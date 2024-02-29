57484a78081295144e4daaf65fe2767a4fbc06b1
昕园 <2269296784@qq.com>
2024-02-29 21:05:46 +0800

    添加 错误 类

---
+++ BowlFrame/Exceptions/Database/DatabaseEx/WriteDataError.cs
@@ -0,0 +1,13 @@
+﻿namespace BowlFrame.Exceptions.Database.DatabaseEx
+{
+    public class WriteDataError : DatabaseException
+    {
+        public WriteDataError() : base("从数据库写入Data结构数据出现错误")
+        {
+        }
+
+        public WriteDataError(string key, string? subkey) : base($"从数据库写入Data结构数据出现错误 key: {key} ${(subkey is not null ? $"subkey: {subkey}" : "")}")
+        {
+        }
+    }
+}
---
+++ BowlFrame/Exceptions/Database/DatabaseException.cs
@@ -0,0 +1,17 @@
+﻿namespace BowlFrame.Exceptions.Database
+{
+    public class DatabaseException : Exception
+    {
+        public DatabaseException() : base("发生数据库异常")
+        {
+        }
+
+        public DatabaseException(string message) : base(message)
+        {
+        }
+
+        public DatabaseException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
