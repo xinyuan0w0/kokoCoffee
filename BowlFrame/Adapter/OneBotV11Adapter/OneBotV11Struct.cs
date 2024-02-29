53a5b88329e859214373c319cfde4cf134dfa49c
昕园 <2269296784@qq.com>
2024-02-29 21:00:20 +0800

    修改细节

--- BowlFrame/Adapter/OneBotV11Adapter/OneBotV11Struct.cs
+++ BowlFrame/Adapter/OneBotV11Adapter/OneBotV11Struct.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BowlFrame.Adapter.OneBotV11Adapter
+﻿namespace BowlFrame.Adapter.OneBotV11Adapter
 {
     internal struct OneBotV11Account
     {
--- BowlFrame/Adapter/OneBotV11Adapter/OneBotV11WS.cs
+++ BowlFrame/Adapter/OneBotV11Adapter/OneBotV11WS.cs
@@ -1,13 +1,8 @@
 ﻿using BowlFrame.Net.WebSocket;
-using static BowlFrame.Tools.Logger;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Newtonsoft.Json.Linq;
 using System.Net.WebSockets;
 using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
-using BowlFrame.Adapter.TencentQQAdapter;
+using static BowlFrame.Tools.Logger;
 
 namespace BowlFrame.Adapter.OneBotV11Adapter
 {
@@ -51,7 +46,7 @@ namespace BowlFrame.Adapter.OneBotV11Adapter
                 string receivedMessage = Encoding.UTF8.GetString(bytes);
                 JObject value = JObject.Parse(receivedMessage);
 
-                Log.Debug(receivedMessage);
+                Log.Trace(receivedMessage);
 
                 string? type = (string?)value["post_type"];
 
