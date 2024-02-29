94780db13219fde2f5f06c0763e90821c3ee9c98
昕园 <2269296784@qq.com>
2024-02-29 21:06:45 +0800

    添加 对象 类

---
+++ BowlFrame/Target/Group.cs
@@ -0,0 +1,21 @@
+﻿using BowlFrame.Message;
+
+namespace BowlFrame.Target
+{
+    internal abstract class Group
+    {
+        public string UUID { get; }
+
+        public Group(string uuid)
+        {
+            UUID = uuid;
+        }
+
+        /// <summary>
+        /// 主动发送
+        /// </summary>
+        /// <param name="messages"></param>
+        /// <returns></returns>
+        public abstract Task<bool> SendAsync(Messages messages);
+    }
+}
---
+++ BowlFrame/Target/Guild.cs
@@ -0,0 +1,24 @@
+﻿using BowlFrame.Message;
+
+namespace BowlFrame.Target
+{
+    public abstract class Guild
+    {
+        public string UUID { get; }
+
+        public string FatherUUID { get; }
+
+        public Guild(string uuid, string fatherUUID)
+        {
+            UUID = uuid;
+            FatherUUID = fatherUUID;
+        }
+
+        /// <summary>
+        /// 主动发送
+        /// </summary>
+        /// <param name="messages"></param>
+        /// <returns></returns>
+        public abstract Task<bool> SendAsync(Messages messages);
+    }
+}
