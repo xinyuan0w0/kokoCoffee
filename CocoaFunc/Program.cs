e7f085c9b81ff7b25da366dd5fa735d6b70027ea
昕园 <2269296784@qq.com>
2024-02-29 21:07:22 +0800

    添加 测试 代码

--- CocoaFunc/Program.cs
+++ CocoaFunc/Program.cs
@@ -5,13 +5,17 @@ namespace CocoaFunc
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
             BowlService.Initialization();
             BowlService.Start();
 
-            EventWaitHandle _waitHandle = new AutoResetEvent(false);
-            _waitHandle.WaitOne();
+            if (Debugger.IsAttached)
+                BowlService.Debug();
+
+            //EventWaitHandle _waitHandle = new AutoResetEvent(false);
+            //_waitHandle.WaitOne();
+            await Task.Delay(-1);
         }
     }
 }
