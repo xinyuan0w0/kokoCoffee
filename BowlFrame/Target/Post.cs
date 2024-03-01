483f4a71feb54d38e5ee59964ff2d2c45eac220b
昕园 <2269296784@qq.com>
2024-03-01 16:16:57 +0800

    规范 对象

---
+++ BowlFrame/Target/Post.cs
@@ -0,0 +1,25 @@
+﻿using BowlFrame.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlFrame.Target
+{
+    /// <summary>
+    /// 预留功能
+    /// </summary>
+    /// <param name="uuid"></param>
+    public abstract class Post(string uuid)
+    {
+        public string UUID { get; } = uuid;
+
+        /// <summary>
+        /// 主动发送
+        /// </summary>
+        /// <param name="messages"></param>
+        /// <returns></returns>
+        public abstract Task<bool> SendAsync(Messages messages);
+    }
+}
