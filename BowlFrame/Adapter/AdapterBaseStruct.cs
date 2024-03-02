056757e63c7e07c9349128fad88238f7533fc647
昕园 <2269296784@qq.com>
2024-03-03 01:10:46 +0800

    添加 适配器基类结构

---
+++ BowlFrame/Adapter/AdapterBaseStruct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlFrame.Adapter
+{
+    internal class BasePlatform : IPlatform
+    {
+        public AdapterInfo AdapterInfo { get; } = new()
+        {
+            Name = "Test",
+            ID = "cn.kokobot.test",
+            Platform = "Test",
+            Description = "测试",
+        };
+
+        public string ID => "Test";
+    }
+}
