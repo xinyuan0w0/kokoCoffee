aff65918433d323d2978e4ea2cec11a6fb30057c
昕园 <2269296784@qq.com>
2024-03-16 02:24:51 +0800

    添加 权限错误 类

---
+++ BowlFrame/Exceptions/Permission/NotFoundTargetPlatform.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlFrame.Exceptions.Permission
+{
+    public class NotFoundTargetPlatform : PermissionException
+    {
+        public NotFoundTargetPlatform() : base("未找到某个对象映射的平台信息")
+        {
+        }
+
+        public NotFoundTargetPlatform(string? uuid) : base($"未找到 {uuid} 对象映射的平台信息 ")
+        {
+        }
+    }
+}
---
+++ BowlFrame/Exceptions/Permission/PermissionException.cs
@@ -0,0 +1,17 @@
+﻿namespace BowlFrame.Exceptions.Permission
+{
+    public class PermissionException : Exception
+    {
+        public PermissionException() : base("发生权限异常")
+        {
+        }
+
+        public PermissionException(string message) : base(message)
+        {
+        }
+
+        public PermissionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
