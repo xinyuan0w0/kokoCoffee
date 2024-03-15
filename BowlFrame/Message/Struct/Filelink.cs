8bf79d913a7a5c685c5c1705f50677805c3fb9fc
昕园 <2269296784@qq.com>
2024-03-16 02:24:27 +0800

    添加 消息块值对象

---
+++ BowlFrame/Message/Struct/Filelink.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlFrame.Message.Struct
+{
+    public struct Filelink
+    {
+        public string MimeType { get; set; }
+
+        public byte[]? Bytes { get; set; }
+
+        public string? Path { get; set; }
+
+        public string? Url { get; set; }
+    }
+}
