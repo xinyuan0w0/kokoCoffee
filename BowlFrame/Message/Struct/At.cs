9bdf2bdb63c851c5fa5fe27f9aca0720f7fc43bd
昕园 <2269296784@qq.com>
2024-03-16 02:24:27 +0800

    添加 消息块值对象

---
+++ BowlFrame/Message/Struct/At.cs
@@ -0,0 +1,18 @@
+﻿using BowlFrame.Adapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlFrame.Message.Struct
+{
+    public struct At
+    {
+        public string? UUID { get; set; }
+
+        public IPlatform Platform { get; set; }
+
+        public string ID { get; set; }
+    }
+}
