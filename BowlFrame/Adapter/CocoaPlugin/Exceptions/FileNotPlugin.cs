5c90badea6234fac22b40478c740c8619ddd703a
昕园 <2269296784@qq.com>
2024-04-08 20:54:05 +0800

    添加 CocoaPlugin 适配器

---
+++ BowlFrame/Adapter/CocoaPlugin/Exceptions/FileNotPlugin.cs
@@ -0,0 +1,17 @@
+﻿namespace BowlFrame.Adapter.CocoaPlugin.Exceptions
+{
+    internal class FileNotPlugin : CocoaPluginException
+    {
+        public FileNotPlugin() : base("加载文件非支持的插件文件")
+        {
+        }
+
+        public FileNotPlugin(string? filePath) : base($"文件 {filePath} 非支持的插件文件")
+        {
+        }
+
+        public FileNotPlugin(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ BowlFrame/Adapter/CocoaPlugin/Exceptions/PluginAlreadyExits.cs
@@ -0,0 +1,21 @@
+﻿namespace BowlFrame.Adapter.CocoaPlugin.Exceptions
+{
+    internal class PluginAlreadyExits : CocoaPluginException
+    {
+        public PluginAlreadyExits() : base("该插件已存在")
+        {
+        }
+
+        public PluginAlreadyExits(string? message) : base(message)
+        {
+        }
+
+        public PluginAlreadyExits(CocoaPluginConfig? config) : base(config is null ? "该插件已存在" : $"{config?.Name} ({config?.ID}) 已存在")
+        {
+        }
+
+        public PluginAlreadyExits(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
