962a1d0ec03c9711187523af133d0861d540b62a
昕园 <2269296784@qq.com>
2024-04-13 15:25:12 +0800

    修改 TencentQQ 富文件上传方法

--- BowlFrame/Adapter/TencentQQAdapter/Tools/UploadMedia.cs
+++ BowlFrame/Adapter/TencentQQAdapter/Tools/UploadMedia.cs
@@ -1,6 +1,7 @@
 ﻿using BowlFrame.Target;
 using BowlFrame.Tools;
 using Newtonsoft.Json.Linq;
+using System.Buffers.Text;
 using System.Net.Http.Json;
 using static BowlFrame.Tools.Tools;
 
@@ -19,24 +20,24 @@ namespace BowlFrame.Adapter.TencentQQAdapter.Tools
         /// <exception cref="NotSupportedException"></exception>
         public static async Task<Files?> GetFiles(TencentQQ tencentQQ, byte[] bytes, short fileType, ITarget target)
         {
-            S3Helper s3 = new();
+            //S3Helper s3 = new();
 
-            string filename = GetMD5Hex(bytes);
+            //string filename = GetMD5Hex(bytes);
 
-            string? url = await s3.GetTempLink(filename);
+            //string? url = await s3.GetTempLink(filename);
 
-            if (url is null || url == "") {
-                if (await s3.UploadFile(filename, bytes) != true)
-                    return null;
-                url = await s3.GetTempLink(filename);
-            }
+            //if (url is null || url == "") {
+            //    if (await s3.UploadFile(filename, bytes) != true)
+            //        return null;
+            //    url = await s3.GetTempLink(filename);
+            //}
 
             HttpRequestMessage request = new(HttpMethod.Post, new Uri(tencentQQ.BaseUrl, target is Group ? $"/v2/groups/{target.ID}/files" : target is User ? $"/v2/users/{target.ID}/files" : throw new NotSupportedException()))
             {
                 Content = JsonContent.Create(new
                 {
                     file_type = fileType,
-                    url,
+                    file_data = Convert.ToBase64String(bytes),
                     srv_send_msg = false,
                 })
             };
