1edc7af10f26b9523e0395a273c2f35c528e2103
昕园 <2269296784@qq.com>
2024-04-13 15:24:21 +0800

    修改 SourceType 位置

--- BowlFrame/Event/EventStruct.cs
+++ BowlFrame/Event/EventStruct.cs
@@ -7,13 +7,4 @@
         Message = 2,
         Timer = 3,
     }
-
-    public enum SourceType
-    {
-        Private = 0,
-        Group = 1,
-        Guild = 2,
-        Channel = 3,
-        Post = 4,
-    }
 }
