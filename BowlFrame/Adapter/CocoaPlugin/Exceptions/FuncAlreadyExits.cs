6b3d62ed1237a63d22df3b2d329d33d0d7949c36
昕园 <2269296784@qq.com>
2024-05-06 16:38:11 +0800

    添加 插件已存在 异常

---
+++ BowlFrame/Adapter/CocoaPlugin/Exceptions/FuncAlreadyExits.cs
@@ -0,0 +1,17 @@
+﻿namespace BowlFrame.Adapter.CocoaPlugin.Exceptions
+{
+    internal class FuncAlreadyExits : CocoaPluginException
+    {
+        public FuncAlreadyExits() : base("该插件已存在")
+        {
+        }
+
+        public FuncAlreadyExits(string? funcID) : base($"{funcID} 已存在")
+        {
+        }
+
+        public FuncAlreadyExits(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
