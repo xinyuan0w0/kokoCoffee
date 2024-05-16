64ea91f9599d5ac4b16e8b367dbec45f374b3082
昕园 <2269296784@qq.com>
2024-05-16 19:47:45 +0800

    添加 事件管理器 功能

---
+++ BowlFrame/Adapter/CocoaPlugin/Event/CocoaEvent.cs
@@ -0,0 +1,112 @@
+﻿using NanoidDotNet;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static BowlFrame.Tools.Logger;
+
+namespace BowlFrame.Adapter.CocoaPlugin.Event
+{
+    internal class CocoaEvent(Delegate @delegate)
+    {
+        ~CocoaEvent()
+        {
+            _bindList.Clear();
+        }
+
+        //绑定列表
+        private readonly ConcurrentDictionary<string, (object, MethodInfo)> _bindList = [];
+
+        //委托
+        private readonly Delegate @delegate = @delegate;
+
+        /// <summary>
+        /// 注册事件
+        /// </summary>
+        /// <param name="object">对象</param>
+        /// <param name="methodInfo">方法信息</param>
+        /// <returns>注册标识符，Null 为添加失败</returns>
+        public string? RegisterEvent(object @object, MethodInfo methodInfo)
+        {
+            try
+            {
+                ParameterInfo[] targetParameters = methodInfo.GetParameters();
+                ParameterInfo[] parameters = @delegate.GetMethodInfo().GetParameters();
+
+                if (targetParameters.Length == parameters.Length)
+                {
+                    for (int i = 0; i < targetParameters.Length; i++)
+                        if (targetParameters[i].ParameterType != parameters[i].ParameterType)
+                            return null;
+
+                    if (methodInfo.ReturnType == @delegate.GetMethodInfo().ReturnType)
+                    {
+                        string flag = Nanoid.Generate(size: 8);
+                        if (_bindList.TryAdd(flag, (@object, methodInfo)))
+                            return flag;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Warn(e);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 取消注册事件
+        /// </summary>
+        /// <param name="flag">标识符</param>
+        /// <returns>结果</returns>
+        public bool UnregisterEvent(string flag)
+        {
+            return _bindList.TryRemove(flag, out _);
+        }
+
+        /// <summary>
+        /// 清空注册列表
+        /// </summary>
+        /// <returns></returns>
+        public void ClearEvent()
+        {
+            _bindList.Clear();
+        }
+
+        /// <summary>
+        /// 触发事件并获取返回值
+        /// </summary>
+        /// <param name="args">参数</param>
+        /// <param name="returns">返回内容</param>
+        /// <returns>执行结果</returns>
+        public bool Invoke(object?[]? args, out object?[]? returns)
+        {
+            returns = null;
+
+            (object, MethodInfo)[] list = [.. _bindList.Values];
+            List<object?> returnList = [];
+
+            try
+            {
+                foreach ((object, MethodInfo) item in list)
+                {
+                    returnList.Add(item.Item2.Invoke(item.Item1, args));
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Warn(e);
+                return false;
+            }
+
+            returns = [.. returnList];
+            return true;
+        }
+    }
+}
---
+++ BowlFrame/Adapter/CocoaPlugin/Event/EventManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlFrame.Adapter.CocoaPlugin.Event
+{
+    public class EventManager
+    {
+        //事件列表
+        private readonly ConcurrentDictionary<string, CocoaEvent> _eventList = [];
+
+        //注册事件列表
+        private readonly ConcurrentDictionary<string, ConcurrentDictionary<int, ConcurrentDictionary<int, string>>> _registerList = [];
+
+        public bool AddEvent(string eventName, Delegate @delegate)
+        {
+            if (_eventList.ContainsKey(eventName))
+                return false;
+
+            CocoaEvent cocoaEvent = new(@delegate);
+
+            if (_eventList.TryAdd(eventName, cocoaEvent) && _registerList.TryAdd(eventName, []))
+                return true;
+
+            _eventList.TryRemove(eventName, out _);
+            _registerList.TryRemove(eventName, out _);
+
+            return false;
+        }
+
+        public bool RemoveEvent(string eventName)
+        {
+            if (!_eventList.ContainsKey(eventName))
+                return false;
+
+            _eventList.TryRemove(eventName, out CocoaEvent? cocoaEvent);
+            _registerList.TryRemove(eventName, out _);
+
+            cocoaEvent?.ClearEvent();
+
+            return true;
+        }
+
+        public bool RegisterEvent(string eventName, object @object, MethodInfo methodInfo)
+        {
+            if (!_eventList.TryGetValue(eventName, out CocoaEvent? cocoaEvent) || !_registerList.TryGetValue(eventName, out ConcurrentDictionary<int, ConcurrentDictionary<int, string>>? dict))
+                return false;
+
+            if (cocoaEvent is null || dict is null)
+                return false;
+
+            //先取出插件的注册列表
+            dict.TryGetValue(@object.GetHashCode(), out ConcurrentDictionary<int, string>? objectDict);
+
+            if (objectDict is null || dict.ContainsKey(methodInfo.GetHashCode()))
+                return false;
+
+            string? flag = cocoaEvent.RegisterEvent(@object, methodInfo);
+
+            if (flag is null)
+                return false;
+
+            return objectDict.TryAdd(methodInfo.GetHashCode(), flag);
+        }
+
+        public bool UnregisterEvent(string eventName, object @object, MethodInfo methodInfo)
+        {
+            if (!_eventList.TryGetValue(eventName, out CocoaEvent? cocoaEvent) || !_registerList.TryGetValue(eventName, out ConcurrentDictionary<int, ConcurrentDictionary<int, string>>? dict))
+                return false;
+
+            if (cocoaEvent is null || dict is null)
+                return false;
+
+            //先取出插件的注册列表
+            dict.TryGetValue(@object.GetHashCode(), out ConcurrentDictionary<int, string>? objectDict);
+
+            if (objectDict is null || !dict.ContainsKey(methodInfo.GetHashCode()))
+                return false;
+
+            objectDict.TryRemove(methodInfo.GetHashCode(), out string? flag);
+
+            if (flag is null)
+                return false;
+
+            return cocoaEvent.UnregisterEvent(flag);
+        }
+    }
+}
