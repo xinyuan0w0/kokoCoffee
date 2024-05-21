ea1bdcd46f15a211a070e1eb0526efa9e09dcaf4
昕园 <2269296784@qq.com>
2024-05-21 23:50:08 +0800

    添加 事件 CheckMatchMessage

---
+++ BowlFrame/Adapter/CocoaPlugin/Event/Manager/CheckMatchMessage.cs
@@ -0,0 +1,11 @@
+﻿using BowlFrame.Event.Message;
+
+namespace BowlFrame.Adapter.CocoaPlugin.Event.Manager
+{
+    public class CheckMatchMessage
+    {
+        public delegate bool CheckMatchMessageHandler(CocoaPluginConfig pluginConfig, CocoaPluginFuncConfig funcConfig, MessageBase messageBase);
+
+        public static bool BindEventManager(EventManager manager) => manager.AddEvent("CheckMatchMessage", typeof(CheckMatchMessageHandler));
+    }
+}
