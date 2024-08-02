53dc19bc0d009245616d8d11b5b27bc7ac3ee069
昕园 <2269296784@qq.com>
2024-08-02 09:53:04 +0800

    添加 ExtraData、RemoveDataError 错误类型

---
+++ BowlFrame/Exceptions/Database/DatabaseEx/ExtraData.cs
@@ -0,0 +1,13 @@
+﻿namespace BowlFrame.Exceptions.Database.DatabaseEx
+{
+    internal class ExtraData : DatabaseException
+    {
+        public ExtraData() : base("从数据库读取Data结构时发现重复数据")
+        {
+        }
+
+        public ExtraData(string key, string? subkey) : base($"从数据库读取Data结构时发现重复数据 key: {key} ${(subkey is not null ? $"subkey: {subkey}" : "")}")
+        {
+        }
+    }
+}
