80fd4472b4e99472cb00b9b8fb447552ef64340d
昕园 <2269296784@qq.com>
2024-08-19 10:24:14 +0800

    添加 初始化传入Cocoa对象

--- BowlFrame/Adapter/CocoaPlugin/ICocoaPlugin.cs
+++ BowlFrame/Adapter/CocoaPlugin/ICocoaPlugin.cs
@@ -2,7 +2,7 @@
 {
     public interface ICocoaPlugin : IDisposable
     {
-        public bool Init(CocoaPluginPath cocoaPluginpath);
+        public bool Init(CocoaPluginPath cocoaPluginpath, Cocoa cocoa);
 
         public bool Enable();
 
